a2547bd894aba5ad3ff94d8873bef431d5555eb3
matheus1714 <matheussilvamartins1714@gmail.com>
2024-08-14 22:55:13 -0300

    refat: apply dependency inversion

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,8 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
-import { registerUseCase } from "@/use-cases/register";
+import { RegisterUsecase } from "@/use-cases/register";
+import { UserAlreadyExistError } from "@/use-cases/errors/user-already-exist-error";
 
 export async function register(req: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,9 +14,16 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(req.body);
 
   try {
-    await registerUseCase({ name, email, password });
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUsecase(prismaUsersRepository);
+
+    await registerUseCase.execute({ name, email, password });
   } catch (err) {
-    return reply.status(409).send();
+    if (err instanceof UserAlreadyExistError) {
+      return reply.status(409).send({ message: err.message });
+    }
+
+    throw err;
   }
 
   return reply.status(201).send();
