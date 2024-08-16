1ab43b603665c53cd0b4e4788b05848a1ad1fa0f
matheus1714 <matheussilvamartins1714@gmail.com>
2024-08-15 22:10:41 -0300

    feat: create use case factories

--- src/use-cases/factories/make-register-use-case.ts
+++ src/use-cases/factories/make-register-use-case.ts
@@ -1,9 +1,9 @@
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
-import { RegisterUsecase } from "../register";
+import { RegisterUseCase } from "../register";
 
 export function makeRegisterUseCase() {
   const prismaUsersRepository = new PrismaUsersRepository();
-  const registerUseCase = new RegisterUsecase(prismaUsersRepository);
+  const registerUseCase = new RegisterUseCase(prismaUsersRepository);
 
   return registerUseCase;
 }
